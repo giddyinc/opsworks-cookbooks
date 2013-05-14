16c40ed82826256d708c70a4f385a3ab287862e4
yifu <yifu@shopgiddy.com>
2013-05-14 02:46:02 -0700

    Use our own package for nodejs

--- opsworks_nodejs/recipes/default.rb
+++ opsworks_nodejs/recipes/default.rb
@@ -1,10 +1,20 @@
 case node[:platform]
 when 'debian', 'ubuntu'
-  execute "Install node.js #{node[:opsworks_nodejs][:version]}" do
-    command "apt-get update; apt-get -y install python-software-properties python g++ make; add-apt-repository -y ppa:chris-lea/node.js; apt-get update; apt-get -y install nodejs"
-
+  remote_file "/tmp/#{node[:opsworks_nodejs][:deb]}" do
+    source node[:opsworks_nodejs][:deb_url]
+    action :create_if_missing
     not_if do
-      ::File.exists?("/usr/local/bin/node")
+      ::File.exists?("/usr/local/bin/node") &&
+      system("/usr/local/bin/node -v | grep -q '#{node[:opsworks_nodejs][:version]}'")
+    end
+  end
+
+  execute "Install node.js #{node[:opsworks_nodejs][:version]}" do
+    cwd "/tmp"
+    command "dpkg -i /tmp/#{node[:opsworks_nodejs][:deb]}"
+
+    only_if do
+      ::File.exists?("/tmp/#{node[:opsworks_nodejs][:deb]}")
     end
   end
 
