b9867089e14868e894827f389f641efa7dafb205
yifu <yifu.wu@gmail.com>
2014-01-30 01:15:47 -0800

    Add app_name and app_version

--- opsworks_dw/attributes/default.rb
+++ opsworks_dw/attributes/default.rb
@@ -12,13 +12,15 @@ default[:dw][:path] = '/opt/dw'
 default[:dw][:s3_bucket] = 'boxed-maven'
 
 # jar
-default[:dw][:jar_filename] = 'pensieve-collector-0.0.1.jar'
-default[:dw][:jar_remote_path] = "/release/com/boxed/pensieve-collector/0.0.1/#{node[:dw][:jar_filename]}"
+default[:dw][:app_name] = ''
+default[:dw][:app_version] = '0.0.1'
+default[:dw][:jar_filename] = "#{node[:dw][:app_name]}-#{node[:dw][:app_version]}.jar"
+default[:dw][:jar_remote_path] = "/release/com/boxed/#{node[:dw][:app_name]}/#{node[:dw][:app_version]}/#{node[:dw][:jar_filename]}"
 default[:dw][:jar_local_file] = "#{node[:dw][:path]}/#{node[:dw][:jar_filename]}"
 
 # config file
 default[:dw][:config_filename] = "config.yml"
-default[:dw][:config_remote_path] = "/release/com/boxed/pensieve-collector/0.0.1/config/#{node[:dw][:config_filename]}"
+default[:dw][:config_remote_path] = "/release/com/boxed/#{node[:dw][:app_name]}/#{node[:dw][:app_version]}/config/#{node[:dw][:config_filename]}"
 default[:dw][:config_local_file] = "#{node[:dw][:path]}/#{node[:dw][:config_filename]}"
 
 # aws - use custom json
