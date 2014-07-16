1f1f49ed597b142c52e68e19b8fbc31bcef67811
William Fong <will@shopgiddy.com>
2014-07-16 15:09:31 -0400

    bower

--- grunt_cookbook/metadata.rb
+++ grunt_cookbook/metadata.rb
@@ -9,3 +9,4 @@ version          "1.0.0"
 # Setup recipes.
 recipe "grunt_cookbook::install_grunt_prereq", "Installs Nodejs and NPM so grunt can do its thing"
 recipe "grunt_cookbook::install_grunt_cli", "Installs the grunt-cli globally using NPM."
+recipe "grunt_cookbook::install_grunt_cli_and_bower", "Installs the grunt-cli and bower globally using NPM."
