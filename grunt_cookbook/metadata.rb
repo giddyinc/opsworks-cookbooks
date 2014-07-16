d6f2b33c5918958df09fbcb903ae790d4a55efc4
William Fong <will@shopgiddy.com>
2014-07-16 14:48:04 -0400

    g

--- grunt_cookbook/metadata.rb
+++ grunt_cookbook/metadata.rb
@@ -5,7 +5,6 @@ description      "Provides lightweight resources and recipes for installing and 
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
-depends "nodejs"
 
 # Setup recipes.
 recipe "grunt_cookbook::install_grunt_prereq", "Installs Nodejs and NPM so grunt can do its thing"
