ecefdf05d9723e907f410b150ca05545df53fa10
will <will@boxed.com>
2014-10-01 12:49:37 -0400

    loggly updates

--- loggly/metadata.rb
+++ loggly/metadata.rb
@@ -1,3 +1,12 @@
-version           "1.0.1"
+name             'loggly-rsyslog'
+maintainer       'Matt Veitas'
+maintainer_email 'mveitas@gmail.com'
+license          'Apache 2.0'
+description      'Configures rsyslog to send logs to Loggly'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '2.1.0'
 
-recipe "loggly", "Installs loggly package"
+supports 'ubuntu', '>= 12.04'
+
+depends "rsyslog", "~> 1.5.0"
+
