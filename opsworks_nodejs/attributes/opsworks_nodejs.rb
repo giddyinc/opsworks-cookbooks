c2a5110b423247364983852b6f444f99b4db59ff
yifu <yifu.wu@gmail.com>
2015-05-26 12:26:17 -0700

    Update nodejs

--- opsworks_nodejs/attributes/opsworks_nodejs.rb
+++ opsworks_nodejs/attributes/opsworks_nodejs.rb
@@ -1,15 +1,31 @@
+###
+# Do not use this file to override the opsworks_nodejs cookbook's default
+# attributes.  Instead, please use the customize.rb attributes file,
+# which will keep your adjustments separate from the AWS OpsWorks
+# codebase and make it easier to upgrade.
+#
+# However, you should not edit customize.rb directly. Instead, create
+# "opsworks_nodejs/attributes/customize.rb" in your cookbook repository and
+# put the overrides in YOUR customize.rb file.
+#
+# Do NOT create an 'opsworks_nodejs/attributes/opsworks_nodejs.rb' in your cookbooks. Doing so
+# would completely override this file and might cause upgrade issues.
+#
+# See also: http://docs.aws.amazon.com/opsworks/latest/userguide/customizing.html
+###
+
 include_attribute 'deploy'
+include_attribute 'opsworks_commons::default'
 
-set[:boxed][:assets_url] = 'https://s3.amazonaws.com/boxed-packages'
-
-set[:opsworks_nodejs][:version] = '0.10.21'
-
-set[:opsworks_nodejs][:pkgrelease] = '1'
+default[:opsworks_nodejs][:version] = '0.10.38'
+default[:opsworks_nodejs][:pkgrelease] = '1'
 
 arch = RUBY_PLATFORM.match(/64/) ? 'amd64' : 'i386'
-set[:opsworks_nodejs][:deb] = "nodejs-#{node[:opsworks_nodejs][:version]}-#{node[:opsworks_nodejs][:pkgrelease]}_#{arch}.deb"
-set[:opsworks_nodejs][:deb_url] = "#{node[:boxed][:assets_url]}/packages/#{node[:platform]}/#{node[:platform_version]}/#{node[:opsworks_nodejs][:deb]}"
+default[:opsworks_nodejs][:deb] = "opsworks-nodejs-#{node[:opsworks_nodejs][:version]}-#{node[:opsworks_nodejs][:pkgrelease]}_#{arch}.deb"
+default[:opsworks_nodejs][:deb_url] = "#{node[:opsworks_commons][:assets_url]}/packages/#{node[:platform]}/#{node[:platform_version]}/#{node[:opsworks_nodejs][:deb]}"
 
 rhel_arch = RUBY_PLATFORM.match(/64/) ? 'x86_64' : 'i686'
-set[:opsworks_nodejs][:rpm] = "nodejs-#{node[:opsworks_nodejs][:version]}-#{node[:opsworks_nodejs][:pkgrelease]}.#{rhel_arch}.rpm"
-set[:opsworks_nodejs][:rpm_url] = "#{node[:boxed][:assets_url]}/packages/#{node[:platform]}/#{node[:platform_version]}/#{node[:opsworks_nodejs][:rpm]}"
+default[:opsworks_nodejs][:rpm] = "opsworks-nodejs-#{node[:opsworks_nodejs][:version]}-#{node[:opsworks_nodejs][:pkgrelease]}.#{rhel_arch}.rpm"
+default[:opsworks_nodejs][:rpm_url] = "#{node[:opsworks_commons][:assets_url]}/packages/#{node[:platform]}/#{node[:platform_version]}/#{node[:opsworks_nodejs][:rpm]}"
+
+include_attribute "opsworks_nodejs::customize"
