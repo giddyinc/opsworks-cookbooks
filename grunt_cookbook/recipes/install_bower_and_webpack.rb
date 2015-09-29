c154035c90afb0ee99b248d9159ff666f7706c2e
William Fong <will@Williams-iMac.local>
2015-09-29 17:37:44 -0400

    no grunt

---
+++ grunt_cookbook/recipes/install_bower_and_webpack.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook: grunt_cookbook
+# install_grunt_cli_and_bower
+#
+# As this is global install just run the command rather than using the npm resource.
+#
+
+grunt_cookbook_npm "/" do
+  action :install
+  package "bower"
+  flags "--global"
+end
+
+grunt_cookbook_npm "/" do
+  action :install
+  package "webpack"
+  flags "--global"
+end
