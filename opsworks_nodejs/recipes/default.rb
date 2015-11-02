a5de7427f17f142e566503a841bace16f1b140e1
yifu <yifu.wu@gmail.com>
2015-11-02 15:33:38 -0800

    Cleanup tmp node files

--- opsworks_nodejs/recipes/default.rb
+++ opsworks_nodejs/recipes/default.rb
@@ -67,3 +67,9 @@ execute "Clean up nodejs files" do
   cwd "/tmp"
   command "rm -f #{node[:opsworks_nodejs][:rpm]} #{node[:opsworks_nodejs][:deb]}"
 end
+
+execute "Clean up tmp npm files" do
+  cwd "/tmp"
+  command "find /tmp/ -type d -cmin +1440 -name 'npm-*' | xargs rm -rf"
+end
+
